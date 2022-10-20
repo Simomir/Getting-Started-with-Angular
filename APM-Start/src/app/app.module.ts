a0b7e40489034d0ad62cae873bb55c8d287dc568
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-20 16:37:17 +0300

    some refactoring

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ProductListComponent} from "./products/product-list.component";
-import {FormsModule} from "@angular/forms";
-import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {StarComponent} from "./shared/star.component";
-import {HttpClientModule} from "@angular/common/http";
+import { ProductListComponent } from "./products/product-list.component";
+import { FormsModule } from "@angular/forms";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+import { StarComponent } from "./shared/star.component";
+import { HttpClientModule } from "@angular/common/http";
 import { ProductDetailComponent } from './products/product-detail.component';
-import {WelcomeComponent} from "./home/welcome.component";
-import {RouterModule} from "@angular/router";
+import { WelcomeComponent } from "./home/welcome.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -25,11 +25,11 @@ import {RouterModule} from "@angular/router";
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', component: ProductDetailComponent},
-      {path: 'welcome', component: WelcomeComponent},
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
     ])
   ],
   bootstrap: [AppComponent]
