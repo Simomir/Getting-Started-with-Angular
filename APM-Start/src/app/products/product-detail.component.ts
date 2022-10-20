4f1e727f899139bd8ea30f8b446b6b3294ee8212
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-20 15:31:34 +0300

    some imports

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'pm-product-detail',
+  // selector is needed if the component is going to be nested
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail'
 
   constructor() { }
 
