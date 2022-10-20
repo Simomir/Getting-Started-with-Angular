5e4796be16a694b31028df2fe06095a95da20bd3
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-20 17:16:20 +0300

    some code for product details

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { IProduct } from "./product";
 
 @Component({
   // selector is needed if the component is going to be nested
@@ -7,7 +8,8 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail'
+  pageTitle: string = 'Product Detail';
+  product: IProduct|undefined;
 
   constructor(private route: ActivatedRoute) { }
 
