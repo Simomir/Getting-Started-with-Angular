0f936acfbfdc1e00a0b864ac55241e6c22fec03c
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-20 15:05:07 +0300

    create new component via cli

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -7,13 +7,15 @@ import {FormsModule} from "@angular/forms";
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 import {StarComponent} from "./shared/star.component";
 import {HttpClientModule} from "@angular/common/http";
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
