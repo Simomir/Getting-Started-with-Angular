b1ed8b4017e99aeaba805ab6fd16e3e083c72f77
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-10-25 15:54:57 +0300

    started refactoring

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from "./product-list.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { StarComponent } from "../shared/star.component";
+import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    StarComponent,
+    ConvertToSpacesPipe
+  ],
   imports: [
     CommonModule
   ]
